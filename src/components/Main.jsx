0623d1ec771f4cd442d3c3c10df85b50b76f6c53
akthernahela <nahela00@gmail.com>
2025-11-10 19:17:51 +0100

    Create the list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -44,12 +44,12 @@ export default function Main() {
                     foodMenu.map((menu) => (
                         <li>
                             <a href="#" className="list-group-item list-group-item-action" aria-current="true">
-                                <div className="d-flex w-100 justify-content-between">
-                                    <h5 className="mb-1">List group item heading</h5>
-                                    <small>3 days ago</small>
+                                <div className="d-flex w-100 justify-content-between" key={menu.id}>
+                                    <h5 className="mb-1">{menu.name}</h5>
+                                    <small>{menu.price} euro</small>
                                 </div>
-                                <p className="mb-1">Some placeholder content in a paragraph.</p>
-                                <small>And some small print.</small>
+                                <p className="mb-1">{menu.description}</p>
+                                <small>{menu.category}</small>
                             </a>
                         </li>))
                 }
